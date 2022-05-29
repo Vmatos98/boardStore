6e527cebf16c71c61c50d6f6c8a82633deff9cfb
Victor Matos <antoniovitomatos@gmail.com>
2022-05-29 16:54:38 -0300

    Games by name

--- src/controllers/gamesControllers.js
+++ src/controllers/gamesControllers.js
@@ -4,17 +4,27 @@ import db from './../db.js';
 
 export async function getGames(req, res) {
     try {
+        const { name } = req.query;
         const games = await db.query(`SELECT games.*, categories.name AS "categoryName"
         FROM games
         JOIN categories
         ON games."categoryId" = categories.id
         `);
+        if(name) {
+            const filteredGames = games.rows.filter(game => game.name.toLowerCase().startsWith(name.toLowerCase()));
+            if(filteredGames.length=== 0){
+                return res.status(404).send('Game not found');
+            }
+            res.send(filteredGames);
+        } else {
         res.send(games.rows);
+        }
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal Server Error');
     }
 }
+
 export async function postGames(req, res){
     try {
         const { name, image, stockTotal, pricePerDay, categoryId } = req.body;
